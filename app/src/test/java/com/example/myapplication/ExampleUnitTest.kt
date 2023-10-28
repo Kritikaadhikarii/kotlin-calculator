ea74e52d1477a2b0bb9e681c4d7af4b5f2450bc2
Kritikaadhikarii <itskritikaaa@gmail.com>
2023-10-28 14:50:57 +0545

    new changes made

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.kotlincalculator
+package com.example.myapplication
 
 import org.junit.Test
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "kotlin calculator"
+rootProject.name = "My Application"
 include(":app")
  
